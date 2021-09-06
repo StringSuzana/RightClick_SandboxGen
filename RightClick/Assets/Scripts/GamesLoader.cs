b063792d3a3cf84a726368baa2f9e266659d2ff7
suzana <antolin.suzana@gmail.com>
2021-09-06 14:12:17 +0200

    garden scene finished for now

--- RightClick/Assets/Scripts/GamesLoader.cs
+++ RightClick/Assets/Scripts/GamesLoader.cs
@@ -10,13 +10,25 @@ public class GamesLoader : MonoBehaviour
     {
         QuizMenu.enabled = false;
     }
+    private void Update()
+    {
+        if (QuizMenu.enabled == true)
+        {
+            //Pause the game (freeze the time)
+            Time.timeScale = 0f;
+        }
+    }
     public void EnterMathQuiz()
     {
+        QuizMenu.enabled = false;
+        Time.timeScale = 1f;
         SceneManager.LoadScene(SceneNames.mathQuiz);
     }
     public void No()
     {
         QuizMenu.enabled = false;
+        Time.timeScale = 1f;
+
         //TODO
         //Move away from the collision area
         //disable moving character on button clicks
