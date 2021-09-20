2ae9e76ffc9f24a78f208d30d33ecf3553ad547e
suzana <antolin.suzana@gmail.com>
2021-09-20 15:07:39 +0200

    how to done

--- RightClick/Assets/Scripts/UI/StartPageManager.cs
+++ RightClick/Assets/Scripts/UI/StartPageManager.cs
@@ -5,8 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class StartPageManager : MonoBehaviour
 {
+    [SerializeField]
+    private Canvas howToMenu;
+    private void Start()
+    {
+        howToMenu.gameObject.SetActive(false);
+    }
     public void StartTheGame()
     {
         SceneManager.LoadScene(SceneNames.GardenScene);
+    }
+    public void CloseHowToMenu()
+    {
+        howToMenu.gameObject.SetActive(false);
+    }
+    public void OpenHowToMenu()
+    {
+        howToMenu.gameObject.SetActive(true);
     }
 }
